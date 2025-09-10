a1fb51a2f5315adf8bc0bf5894efe081abac6914
htl3r-2128 <2128@htl.rennweg.at>
2025-09-10 16:38:18 +0200

    Elif Aufgaben

---
+++ Python_Kurs/Python_Kurs.py
@@ -0,0 +1,22 @@
+# lege eine neue Variable mit der Bezeichnung x and und schreibe die
+# Ganzzahl 5 hinein
+x = 9
+y = 7
+
+# prüfe einen komplexere Kette an Bedingungen und reagiere entsprechend
+if x == 7:
+    print("x ist 7")
+elif y == 7:
+    print("y ist 7")
+elif x > 5 and y == 7:
+    print("x ist größer als 5 und y ist 7")
+elif x == 5 and y > 1:
+    print("x ist größer als 1 und y ist größer als 1")
+elif x > 0 and y > 0:
+    print("x ist größer als 0 und y ist größer als 0")
+else:
+    print("Keine der vorigen Bedingungen trifft zu!")
+
+# Probier's selbst aus:
+# Setzte verschiedene Zahlen für x und y ein und
+# schau dir an was passiert.
