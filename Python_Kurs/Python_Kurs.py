296237a37265eb38a2a1604b57a1164d3d5286cf
htl3r-2128 <2128@htl.rennweg.at>
2025-09-11 11:52:27 +0200

    Dictioaries

--- Python_Kurs/Python_Kurs.py
+++ Python_Kurs/Python_Kurs.py
@@ -146,3 +146,112 @@ while len(unsortierteListe) != 0:
 # gebe das Ergebnis aus, es sollten die Zahlen 0 bis 9 in aufsteigender
 # Reihenfolge angezeigt werden, dann ist dein Algorithmus korrekt
 print(sortierteListe)
+
+
+#Dictionaries
+
+# lege ein Dictionary mit Daten einer Kundin an
+kunde1 = {
+    "vorname": "Sarah",
+    "nachname": "Meier",
+    "straße": "Goethestraße",
+    "hausnummer": 4,
+    "telefonnummer": "07315/642735",
+    "kontostand": 11.35,
+    "istAktivKunde": True
+}
+
+# lege noch ein Dictionary mit Daten eines Kunden an
+kunde2 = {
+    "vorname": "Markus",
+    "nachname": "Schreiber",
+    "straße": "Lindenstraße",
+    "hausnummer": 55,
+    "telefonnummer": "03355/392624",
+    "kontostand": 4.56,
+    "istAktivKunde": False
+}
+
+# gebe einige der Werte aus
+print(kunde1["vorname"])
+print(kunde2["kontostand"])
+print(kunde1["telefonnummer"])
+
+# Probier's selbst aus:
+# Lass dir den Nachnamen, die Straße und den Kontostand von Markus ausgeben.
+print(kunde2["nachname"])
+print(kunde2["straße"])
+print(kunde2["kontostand"])
+
+
+# lege zwei Dictionaries an
+kunde1 = {
+        "vorname": "Sarah",
+        "nachname": "Meier",
+     }
+
+kunde2 = {
+        "vorname": "Markus",
+        "nachname": "Schreiber",
+     }
+
+# lösche einen Key und den dazugehörigen Wert aus einem Dictionary
+del kunde1["vorname"]
+
+# leere das andere Dictionary
+kunde2.clear();
+
+# Probier's selbst aus:
+# Füge deine Daten im Dictionary "kunde1" hinzu.
+# Gebe dann das Dictionary aus.
+kunde1["vorname"] = "Paul"
+kunde1["nachname"] = "Bauer"
+
+# gebe beide Dictionaries aus
+print(kunde1)
+print(kunde2)
+
+
+# Aufgabe: Umgekehrtes Mapping
+
+# gegeben ist ein Dictionary, das pro Kontinent eine Liste verschiedener
+# Länder enthält
+kontinentZuLänderMapping = {
+    "Afrika": ["Kongo", "Sudan", "Südafrika", "Marokko"],
+    "Asien": ["Japan", "Indonesien", "Indien", "Bangladesch"],
+    "Amerika": ["Kanada", "Brasilien", "Chile", "Uruguay"],
+    "Europa": ["Frankreich", "Polen", "Italien", "Slowenien"],
+    "Ozeanien": ["Australien", "Neuseeland", "Papua-Neuguinea", "Samoa"]
+}
+
+# dieses Mapping soll nun umgekehrt werden, so dass die Daten nach
+# folgendem Format vorliegen:
+# {
+#   "Kongo": "Afrika",
+#   "Sudan": "Afrika",
+#   ....
+#   "Papua-Neuguinea": "Ozeanien",
+#   "Samoa": "Ozeanien"
+# }
+# schreibe das Mapping in dieser Form in dieses Dictionary
+länderZuKontinentMapping = {}
+
+# gehe alle Schlüssel (also alle Kontinente) durch
+for key in kontinentZuLänderMapping:
+    # hole die Liste der Länder von jedem Kontinent
+    laenderListe = kontinentZuLänderMapping[key]
+    # gehe diese Liste der Länder durch
+    for land in laenderListe:
+        # schreibe jedes Land als Schlüssel und den dazugehörigen
+        # Kontinent als Wert in das Ziel-Dictionary
+        länderZuKontinentMapping[land] = key
+
+# diese Funktion gibt ein Dictionary hübsch formatiert auf
+# der Konsole aus
+def gibDictionaryLesbarAus(d):
+    import json
+    print(json.dumps(d, sort_keys=False, ensure_ascii=False, indent=4))
+
+# das gegebene und das erstellte Dictionary werden ausgegeben
+gibDictionaryLesbarAus(kontinentZuLänderMapping)
+gibDictionaryLesbarAus(länderZuKontinentMapping)
